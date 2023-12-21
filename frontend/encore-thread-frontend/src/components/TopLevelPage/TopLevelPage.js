ab92574f96b3c383602fb3eb6f167943461711f6
Ty11347 <ty11347@gmail.com>
2023-12-21 22:34:08 +0800

    add top level page

---
+++ frontend/encore-thread-frontend/src/components/TopLevelPage/TopLevelPage.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+const Sidebar = () => {
+  return (
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/products">Products</a></li>
+
+        </ul>
+      </nav>
+  );
+};
+
+const TopLevelPage = () => {
+  return (
+      <Router>
+        <div style={{ display: 'flex' }}>
+          <div style={{ width: '20%', background: '#f0f0f0' }}>
+            <Sidebar />
+          </div>
+          <div style={{ width: '80%' }}>
+            <main>
+              <Routes>
+                {/*here to add page components*/}
+                <Route exact path="/" element={<HomeComponent />} />
+                <Route path="/products" element={<ProductsComponent />} />
+
+              </Routes>
+            </main>
+          </div>
+        </div>
+      </Router>
+  );
+};
+
+// temp use
+const HomeComponent = () => <div>Home Page Component</div>;
+const ProductsComponent = () => <div>Products Page Component</div>;
+
+export default TopLevelPage;
