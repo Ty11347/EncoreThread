c27e67f77fdd1e32f53bc14622b2087e7efdbbe7
Ty11347 <ty11347@gmail.com>
2023-12-21 22:46:36 +0800

    set height top level page height to 100vh

--- frontend/encore-thread-frontend/src/components/TopLevelPage/TopLevelPage.js
+++ frontend/encore-thread-frontend/src/components/TopLevelPage/TopLevelPage.js
@@ -17,7 +17,7 @@ const Sidebar = () => {
 const TopLevelPage = () => {
   return (
       <Router>
-        <div style={{ display: 'flex' }}>
+        <div style={{ display: 'flex', height: "100vh" }}>
           <div style={{ width: '20%', background: '#f0f0f0' }}>
             <Sidebar />
           </div>
