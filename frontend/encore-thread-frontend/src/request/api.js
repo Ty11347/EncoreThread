667bc30b03b1e4a06f50b959bcf8cb7dc13ab75d
Ty11347 <ty11347@gmail.com>
2023-12-27 09:48:33 +0800

    edit comments

--- frontend/encore-thread-frontend/src/request/api.js
+++ frontend/encore-thread-frontend/src/request/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://api.example.com', // api address
+  // baseURL: 'https://api.example.com', // base url api address
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
--- frontend/encore-thread-frontend/src/setupProxy.js
+++ frontend/encore-thread-frontend/src/setupProxy.js
@@ -1,12 +1,15 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const {createProxyMiddleware} = require('http-proxy-middleware')
 
-module.exports = function (app){
+module.exports = function (app) {
   app.use(
+      // request url start with '/api' will trigger this proxy, '/api' will be auto removed
+      // eg: 'http://localhost:8080/user/id' => '/api/user/id'
+
       '/api',
       createProxyMiddleware({
         target: "http://localhost:8080",
         changeOrigin: true,
-        pathRewrite(path){
+        pathRewrite(path) {
           return path.replace('/api', '')
         }
       })
