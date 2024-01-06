061b77dc8529b8f0274685de7934d890951dfad4
leeyiwan <leeyiwan2001@gmail.com>
2024-01-06 22:22:53 +0800

    feat order service impl

---
+++ src/main/java/com/cbse/encorethread/impl/OrderDetailsServiceImpl.java
@@ -0,0 +1,64 @@
+package com.cbse.encorethread.impl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cbse.encorethread.dto.OrderDetailsDTO;
+import com.cbse.encorethread.model.OrderDetails;
+import com.cbse.encorethread.model.Products;
+import com.cbse.encorethread.repository.OrderDetailsRepository;
+import com.cbse.encorethread.repository.ProductsRepository;
+import com.cbse.encorethread.service.OrderDetailsService;
+
+@Service
+public class OrderDetailsServiceImpl implements OrderDetailsService {
+  @Autowired
+  private OrderDetailsRepository orderDetailsRepository;
+
+  @Autowired
+  private ProductsRepository productsRepository;
+
+  private OrderDetailsDTO orderDetailsDTOMapper(OrderDetails orderDetails) {
+    Products product = productsRepository.getById(orderDetails.getProductId());
+
+    return new OrderDetailsDTO(
+      product.getId(),
+      orderDetails.getQuantity(),
+      orderDetails.getPrice(),
+      product.getName(),
+      product.getImageUrl(),
+      product.getDescription()
+    );
+  }
+
+  public OrderDetails getOrderDetailById(Integer id) {
+    return orderDetailsRepository.findById(id).orElseThrow(() -> new RuntimeException("OrderDetails not found with id: " + id));
+  }
+
+  public List<OrderDetailsDTO> getOrderDetailsByOrderId(Integer orderId) {
+    return orderDetailsRepository.findAllByOrderId(orderId)
+        .stream()
+        .map(orderDetails -> orderDetailsDTOMapper(orderDetails))
+        .collect(Collectors.toList());
+  }
+
+  public OrderDetails createOrderDetails(OrderDetails orderDetails) {
+    Optional<Integer> temp = orderDetailsRepository.findLatestId();
+    Integer orderDetailId;
+    if (temp.isEmpty()) {
+      orderDetailId = 0;
+    } else {
+      orderDetailId = temp.get();
+    }
+    orderDetails.setId(++orderDetailId);
+    return orderDetailsRepository.save(orderDetails);
+  }
+
+  public void deleteOrderDetails(Integer id) {
+    orderDetailsRepository.deleteById(id);
+  }
+}
--- src/main/java/com/cbse/encorethread/service/OrdersService.java
+++ src/main/java/com/cbse/encorethread/service/OrdersService.java
@@ -1,110 +1,27 @@
 package com.cbse.encorethread.service;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cbse.encorethread.dto.OrdersDTO;
-import com.cbse.encorethread.model.OrderDetails;
 import com.cbse.encorethread.model.Orders;
-import com.cbse.encorethread.repository.OrderDetailsRepository;
-import com.cbse.encorethread.repository.OrdersRepository;
 
 @Service
-public class OrdersService {
-  private final OrdersRepository ordersRepository;
+public interface OrdersService {
+  public List<Orders> getAllOrders();
 
-  @Autowired
-  private OrderDetailsRepository orderDetailsRepository;
+  public Orders getOrderById(Integer id);
 
-  public OrdersService(OrdersRepository ordersRepository) {
-    this.ordersRepository = ordersRepository;
-  }
+  public List<Orders> getOrdersByUserId(Long userId);
 
-  public List<Orders> getAllOrders() {
-    List<Orders> orders = ordersRepository.findAll();
-    orders.sort((o1, o2) -> o1.getId() - o2.getId());
-    return orders;
-  }
+  public Orders createOrder(Orders order);
 
-  public Orders getOrderById(Integer id) {
-    return ordersRepository.findById(id).get();
-  }
+  public Orders createOrderByDTO(OrdersDTO orderdto);
 
-  public List<Orders> getOrdersByUserId(Long userId) {
-    List<Orders> orders = ordersRepository.findByUserId(userId);
-    orders.sort((o1, o2) -> o1.getId() - o2.getId());
-    return orders;
-  }
+  public void deleteOrderById(Integer id);
 
-  public Orders createOrder(Orders order) {
-    Optional<Integer> temp = ordersRepository.findLatestId();
-    Integer orderId;
-    if (temp.isEmpty()) {
-      orderId = 0;
-    } else {
-      orderId = temp.get();
-    }
-    order.setId(++orderId);
-    if (order.getId() == null) {
-      order.setId(1);
-    }
-    return ordersRepository.save(order);
-  }
+  public void deleteOrderByUserId(Long userId);
 
-  public Orders createOrderByDTO(OrdersDTO orderdto) {
-    Orders order = new Orders(
-      orderdto.getUserId(),
-      orderdto.getOrderStatus(),
-      orderdto.getTotalPrice(),
-      orderdto.getAddress(),
-      orderdto.getOrderDate()
-    );
-    Optional<Integer> temp = ordersRepository.findLatestId();
-    Integer orderId;
-    if (temp.isEmpty()) {
-      orderId = 0;
-    } else {
-      orderId = temp.get();
-    }
-    order.setId(++orderId);
-    order = ordersRepository.save(order);
-    OrderDetails[] orderDetails = new OrderDetails[orderdto.getProductIds().length];
-    for (int i = 0; i < orderdto.getProductIds().length; i++) {
-      orderDetails[i] = new OrderDetails(
-        order.getId(),
-        orderdto.getProductIds()[i],
-        orderdto.getQuantities()[i],
-        orderdto.getPrices()[i]
-      );
-      temp = orderDetailsRepository.findLatestId();
-      Integer orderDetailsId;
-      if (temp.isEmpty()) {
-        orderDetailsId = 0;
-      } else {
-        orderDetailsId = temp.get();
-      }
-      orderDetails[i].setId(++orderDetailsId);
-      orderDetailsRepository.save(orderDetails[i]);
-    }
-    return order;
-  }
-
-  public void deleteOrderById(Integer id) {
-    orderDetailsRepository.deleteByOrderId(id);
-    ordersRepository.deleteById(id);
-  }
-
-  public void deleteOrderByUserId(Long userId) {
-    for (Orders order : ordersRepository.findByUserId(userId)) {
-      orderDetailsRepository.deleteByOrderId(order.getId());
-    }
-    ordersRepository.deleteByUserId(userId);
-  }
-
-  public Orders updateOrder(Orders order) {
-    return ordersRepository.save(order);
-  } 
+  public Orders updateOrder(Orders order);
 }
