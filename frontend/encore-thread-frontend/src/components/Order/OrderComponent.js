0c4a1de31ef8bf47273a47a91923926747a07e33
Ty11347 <ty11347@gmail.com>
2024-01-07 04:13:21 +0800

    fix cancel order quantity issue

--- frontend/encore-thread-frontend/src/components/Order/OrderComponent.js
+++ frontend/encore-thread-frontend/src/components/Order/OrderComponent.js
@@ -12,6 +12,57 @@ function OrderComponent({ showingHistory }) {
   const [isCanceling, setIsCanceling] = useState(false)
   const [cancelId, setCancelId] = useState('')
   const [isSuccess, setIsSuccess] = useState(false)
+  const [orderItems, setOrderItems] = useState([])
+
+  const fetchOrderDetails = async (orderId) => {
+    try {
+      const response = await fetch(`http://localhost:8080/api/orderdetails/order/${orderId}`);
+      const data = await response.json();
+      setOrderItems(data);
+      return data;
+    } catch (error) {
+      console.log("Error fetching order items: ", error);
+      return [];
+    }
+  };
+
+  const handleProductQuantityChange = async (orderDetails) => {
+    try {
+      for (const detail of orderDetails) {
+        const quantityChange = detail.quantity;
+        const response = await fetch(
+            `http://localhost:8080/api/admin/products/quantity/${detail.productId}?quantityChange=${quantityChange}`,
+            {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+        );
+
+        // if stock insufficient
+        if (response.status === 409) {
+          const availableQuantity = await response.text();
+          console.error("Insufficient stock for product ID:", detail.productId);
+          alert(
+              "Insufficient stock for product ID " +
+              detail.productId +
+              ": " +
+              availableQuantity
+          );
+          return;
+        } else if (!response.ok) {
+          console.error(
+              "Error updating product quantity for product ID:",
+              detail.productId
+          );
+          return;
+        }
+      }
+    } catch (e) {
+      console.error("Error updating product quantity:", e);
+    }
+  };
 
   useEffect(() => {
     fetch(`http://localhost:8080/api/orders/user/${userId}`)
@@ -64,9 +115,12 @@ function OrderComponent({ showingHistory }) {
             {
               isCanceling &&
               <form className="cancel-order-form"
-                onSubmit={(e) => {
+                onSubmit={async (e) => {
                   e.preventDefault()
                   const order = orders.filter((order) => order.id === parseInt(cancelId))[0]
+                  const orderDetails = await fetchOrderDetails(order.id)
+                  console.log(orderDetails);
+
                   if (order.orderStatus === ORDER_STATUS_PENDING || order.orderStatus === ORDER_STATUS_PROCESSING) {
                     fetch(`http://localhost:8080/api/orders/update`, {
                       method: 'PUT',
@@ -79,8 +133,10 @@ function OrderComponent({ showingHistory }) {
                       })
                     })
                       .then((response) => response.json())
-                      .then((data) => {
+                      .then(async (data) => {
+                        console.log(order);
                         setOrders(prev => prev.map((order) => order.id === data.id ? data : order))
+                        await handleProductQuantityChange(orderDetails);
                         setIsSuccess(true)
                         setTimeout(() => setIsSuccess(false), 3000)
                       })
@@ -99,14 +155,14 @@ function OrderComponent({ showingHistory }) {
                   }
                 </select>
                 <label htmlFor="cancelReason">Reason for Cancellation</label>
-                <textarea name="cancelReason" id="cancelReason" cols="30" rows="10"></textarea>
+                <textarea name="cancelReason" id="cancelReason" cols="30" rows="10"/>
                 <button className="cancel-order-button" type='submit'>Submit</button>
               </form>
             }
           </div>
           {
             isSuccess &&
-            <SuccessMessage message={"Order cancelled successfully"}></SuccessMessage>
+            <SuccessMessage message={"Order cancelled successfully"}/>
           }
         </div>
       </div>
