c4432436e731bad77188f2821fbcc0fdf47aa923
Ty11347 <ty11347@gmail.com>
2024-01-07 00:43:50 +0800

    add service impl

---
+++ src/main/java/com/cbse/encorethread/impl/AdminServiceImpl.java
@@ -0,0 +1,44 @@
+package com.cbse.encorethread.service;
+
+import com.cbse.encorethread.model.Products;
+import com.cbse.encorethread.repository.ProductsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+
+    private final ProductsRepository productsRepository;
+
+    public AdminServiceImpl(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
+
+    @Override
+    public Products addProduct(Products product) {
+        return productsRepository.save(product);
+    }
+
+    @Override
+    public Products updateProduct(Integer id, Products product) {
+        product.setId(id);
+        return productsRepository.save(product);
+    }
+
+    @Override
+    public void deleteProduct(Integer id) {
+        productsRepository.deleteById(id);
+    }
+
+    @Override
+    public int updateProductQuantity(Integer id, Integer quantityChange) throws Exception {
+        Products product = productsRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+        int newQuantity = product.getQuantity() + quantityChange;
+        if (newQuantity < 0) {
+            throw new IllegalStateException("Insufficient stock");
+        }
+        product.setQuantity(newQuantity);
+        productsRepository.save(product);
+        return newQuantity;
+    }
+}
--- src/main/java/com/cbse/encorethread/service/AdminService.java
+++ src/main/java/com/cbse/encorethread/service/AdminService.java
@@ -1,40 +1,14 @@
 package com.cbse.encorethread.service;
 
 import com.cbse.encorethread.model.Products;
-import com.cbse.encorethread.repository.ProductsRepository;
-import org.springframework.stereotype.Service;
 
-@Service
-public class AdminService {
+public interface AdminService {
 
-    private final ProductsRepository productsRepository;
+    Products addProduct(Products product);
 
-    public AdminService(ProductsRepository productsRepository) {
-        this.productsRepository = productsRepository;
-    }
+    Products updateProduct(Integer id, Products product);
 
-    public Products addProduct(Products product) {
-        return productsRepository.save(product);
-    }
+    void deleteProduct(Integer id);
 
-    public Products updateProduct(Integer id, Products product) {
-        product.setId(id);
-        return productsRepository.save(product);
-    }
-
-    public void deleteProduct(Integer id) {
-        productsRepository.deleteById(id);
-    }
-
-    public int updateProductQuantity(Integer id, Integer quantityChange) throws Exception {
-        Products product = productsRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Product not found"));
-        int newQuantity = product.getQuantity() + quantityChange;
-        if (newQuantity < 0) {
-            throw new IllegalStateException("Insufficient stock");
-        }
-        product.setQuantity(newQuantity);
-        productsRepository.save(product);
-        return newQuantity;
-    }
+    int updateProductQuantity(Integer id, Integer quantityChange) throws Exception;
 }
