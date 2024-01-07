1b0e1027d08179a84bd8b73d14f668265d1f0aed
Ty11347 <ty11347@gmail.com>
2024-01-08 00:17:57 +0800

    add admin repository

--- src/main/java/com/cbse/encorethread/repository/ProductsRepository.java
+++ src/main/java/com/cbse/encorethread/repository/ProductsRepository.java
@@ -2,6 +2,12 @@ package com.cbse.encorethread.repository;
 
 import com.cbse.encorethread.model.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductsRepository extends JpaRepository<Products, Integer>{
+
+    @Modifying
+    @Query(value = "UPDATE products SET quantity = quantity + :quantityChange WHERE product_id = :id", nativeQuery = true)
+    int updateProductQuantityWithSql(Integer id, Integer quantityChange);
 }
